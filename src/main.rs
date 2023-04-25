2062ec770a4b86ca7eaae9b7b8073f85cf0f2891
0XRuFFy <66907424+0xRuFFy@users.noreply.github.com>
2023-04-25 11:05:49 +0200

    changed rom

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ const SCALE: u32 = 10;
 
 fn main() {
 
-    let rom = fs::read("ch8/tictactoe.ch8").expect("Unable to read file");
+    let rom = fs::read("ch8/ibm_logo.ch8").expect("Unable to read file");
     println!("rom size: {}b", rom.len());
     
     let mut cpu = C8Cpu::new();
